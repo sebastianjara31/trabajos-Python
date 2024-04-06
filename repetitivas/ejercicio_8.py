0a8d85ddeaf70ae871cd5094e9a2e61228576881
sebastianjara31 <150764394+sebastianjara31@users.noreply.github.com>
2024-04-05 19:53:20 -0500

    ejercicio_8.py

--- repetitivas/ejercicio_8.py
+++ repetitivas/ejercicio_8.py
@@ -1,20 +1,32 @@
 import random 
 valorCompra=int(input("se ingrasa el valor de la compra: "))
-aleatorio = random.choice(["con la valota roja", "cion la balota verde", "con la balota azul", "con la balota amarrilla", "con la balota negra"])
-if valorCompra>=50000 and balota=="rojo":
+balota= random.choice(["roja", "verde", "azul", "amarrilla", "negra"])
+descuento=0
+if valorCompra>=50000 and balota=="roja":
     descuento=0.10
-    print(valorCompra*descuento)
+    print("balota elegida es roja")
+    print("El descuento es de",valorCompra*descuento)
+    print("El valor a pagar es: ",valorCompra-(valorCompra*descuento))
 elif valorCompra>=50000 and balota=="verde":
     descuento=0.15
-    print(valorCompra*descuento)
+    print("balota elegida es verde")
+    print("El descuento es de",valorCompra*descuento)
+    print("El valor a pagar es: ",valorCompra-(valorCompra*descuento))
 elif valorCompra>=50000 and balota=="azul":
     descuento=0.20
-    print(valorCompra*descuento)
+    print("balota elegida es azul")
+    print("El descuento es de",valorCompra*descuento)
+    print("El valor a pagar es: ",valorCompra-(valorCompra*descuento))
 elif valorCompra>=50000 and balota=="amarilla":
     descuento=0.50
-    print(valorCompra*descuento)
-elif valorCompra>=50000 and balota== "negra":
+    print("balota elegida es amarilla")
+    print("El descuento es de",valorCompra*descuento)
+    print("El valor a pagar es: ",valorCompra-(valorCompra*descuento))
+elif valorCompra>=50000 and balota=="negra":
     descuento=100
-    print(valorCompra*descuento)
+    print("balota elegida es negra")
+    print("El descuento es de",valorCompra*descuento)
+    print("El valor a pagar es: ",valorCompra-(valorCompra*descuento))
 elif valorCompra<=50000:
     print("no participas")
+    print("El valor a pagar es: ",valorCompra-(valorCompra*descuento))
